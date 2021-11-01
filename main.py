095c03a9a3a7e64c6217cb23629d785de89498e8
dzakharo <zakharov.dimitry@gmail.com>
2021-11-01 15:49:10 +0300

    requirements.txt added, main returns dummy data

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import fastapi
+import uvicorn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = fastapi.FastAPI()
 
 
-# Press the green button in the gutter to run the script.
+@app.get('/')
+def index():
+    return "Financial Supporter"
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    uvicorn.run(app)
